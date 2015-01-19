43d25a7b5dbbd7dd98aee8d9b36cbcb21ef5e87b
sam.burns <sam.burns@affiliatewindow.com>
2015-01-19 15:48:50 +0000

    Merging trees works

--- src/SamBurns/Tree.php
+++ src/SamBurns/Tree.php
@@ -3,5 +3,59 @@ namespace SamBurns;
 
 class Tree
 {
+    /**
+     * @var array
+     */
+    private $nodes;
 
+    /**
+     * @param array|null $initialArray
+     */
+    public function __construct($initialArray = array())
+    {
+        $this->nodes = array();
+
+        if ($initialArray) {
+
+            foreach ($initialArray as $key => $initialArrayElement) {
+                if (is_array($initialArrayElement)) {
+                    $this->nodes[$key] = new Tree($initialArrayElement);
+                } else {
+                    $this->nodes[$key] = $initialArrayElement;
+                }
+            }
+        }
+    }
+
+    /**
+     * @return array
+     */
+    public function toArray()
+    {
+        $arrayForm = array();
+
+        foreach ($this->nodes as $key => $node) {
+            if ($node instanceof Tree) {
+                $arrayForm[$key] = $node->toArray();
+            } else {
+                $arrayForm[$key] = $node;
+            }
+        }
+
+        return $arrayForm;
+    }
+
+    /**
+     * @param array $arrayToMergeIn
+     * @return Tree
+     */
+    public function addArray($arrayToMergeIn)
+    {
+        return new Tree(
+            array_merge_recursive(
+                $this->toArray(),
+                $arrayToMergeIn
+            )
+        );
+    }
 }
--- tests/phpspec/TreeSpec/SamBurns/TreeSpec.php
+++ tests/phpspec/TreeSpec/SamBurns/TreeSpec.php
@@ -3,11 +3,47 @@ namespace TreeSpec\SamBurns;
 
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
+use SamBurns\Tree;
 
 class TreeSpec extends ObjectBehavior
 {
+    function let()
+    {
+        $initialArray = array(
+            'node' => array(
+                'initial-leaf-node' => 123
+            )
+        );
+
+        $this->beConstructedWith($initialArray);
+    }
+
     function it_is_initializable()
     {
         $this->shouldHaveType('SamBurns\Tree');
+    }
+
+    function it_can_be_converted_to_array()
+    {
+        $expectedArray = array(
+            'node' => array(
+                'initial-leaf-node' => 123
+            )
+        );
+
+        $this->toArray()->shouldBeLike($expectedArray);
+    }
+
+    function it_can_have_array_added()
+    {
+        $dataArray = array('node' => array('new-leaf-node' => 456));
+        $expectedResultArray = array(
+            'node' => array(
+                'new-leaf-node'     => 456,
+                'initial-leaf-node' => 123
+            )
+        );
+
+        $this->addArray($dataArray)->shouldBeLike(new Tree($expectedResultArray));
     }
 }
