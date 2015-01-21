0e42969e54dfdb09d380d2581657b05bcec4cdaf
sam.burns <sam.burns@affiliatewindow.com>
2015-01-21 15:58:10 +0000

    Adding Interface

--- src/SamBurns/Tree/ConfigFromFile.php
+++ src/SamBurns/Tree/ConfigFromFile.php
@@ -1,6 +1,33 @@
 <?php
 namespace SamBurns\Tree;
 
+use SamBurns\Tree\FileParsing\File\Exception\CannotParseFileType;
+use SamBurns\Tree\FileParsing\File\Exception\FileDoesNotExist;
+
 interface ConfigFromFile
 {
+    /**
+     * @return array
+     */
+    public function toArray();
+
+    /**
+     * @param array $arrayToMergeIn
+     * @return Config
+     */
+    public function mergeWithArray($arrayToMergeIn);
+
+    /**
+     * @throws CannotParseFileType
+     * @throws FileDoesNotExist
+     *
+     * @param string $path
+     */
+    public function populateFromFile($path);
+
+    /**
+     * @param Config $config
+     * @return Config
+     */
+    public function mergeWithConfig(Config $config);
 }
